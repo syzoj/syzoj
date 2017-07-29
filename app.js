6cba999a52c5791b2eeb721c62617e20b7791bcf
Menci <HuangHaoRui301@Gmail.com>
2017-07-29 12:55:54 +0800

    Fix no res.locals.user in api_v2

--- app.js
+++ app.js
@@ -61,6 +61,8 @@ global.syzoj = {
     let multer = require('multer');
     app.multer = multer({ dest: syzoj.utils.resolvePath(syzoj.config.upload_dir, 'tmp') });
 
+    // This should before load api_v2, to init the `res.locals.user`
+    this.loadHooks();
     // Trick to bypass CSRF for APIv2
     app.use((() => {
       let router = new Express.Router();
@@ -73,7 +75,6 @@ global.syzoj = {
     app.use(csurf({ cookie: true }));
 
     await this.connectDatabase();
-    this.loadHooks();
     this.loadModules();
   },
   async connectDatabase() {
