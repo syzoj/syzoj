09be8367d85896b03c0a4ba0a2d17af7fdb9a562
ningyuv <ningyuv@outlook.com>
2018-11-09 19:44:41 +0800

    disallow view testdata

--- modules/problem.js
+++ modules/problem.js
@@ -713,6 +713,7 @@ app.get('/problem/:id/testdata', async (req, res) => {
 
     if (!problem) throw new ErrorMessage('无此题目。');
     if (!await problem.isAllowedUseBy(res.locals.user)) throw new ErrorMessage('您没有权限进行此操作。');
+    if (!await problem.isAllowedEditBy(res.locals.user)) throw new ErrorMessage('您没有权限进行此操作。');
 
     let testdata = await problem.listTestdata();
     let testcases = await syzoj.utils.parseTestdata(problem.getTestdataPath(), problem.type === 'submit-answer');
