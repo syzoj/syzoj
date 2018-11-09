4b968da87be876327d8f7354eead0fb9f5bf64e4
ningyuv <ningyuv@outlook.com>
2018-11-09 22:40:29 +0800

    view own code in contests

--- modules/contest.js
+++ modules/contest.js
@@ -121,9 +121,6 @@ app.get('/contest/:id', async (req, res) => {
     if (!contest) throw new ErrorMessage('无此比赛。');
     if (!contest.is_public && (!res.locals.user || !res.locals.user.is_admin)) throw new ErrorMessage('比赛未公开，请耐心等待 (´∀ `)');
     if (!await contest.isSupervisior(curUser) && !(contest.isRunning() || contest.isEnded())) {
-      if (await problem.isAllowedUseBy(res.locals.user)) {
-        return res.redirect(syzoj.utils.makeUrl(['problem', problem_id]));
-      }
       throw new ErrorMessage('比赛尚未开始。');
     }
     const isSupervisior = await contest.isSupervisior(curUser);
