71e2183b84bc420df447525ae8f7cd28302c8fde
xuhanxiang <xuhanxiang@megvii.com>
2019-03-21 15:20:54 +0800

    1

--- modules/problem.js
+++ modules/problem.js
@@ -630,7 +630,8 @@ app.post('/problem/:id/submit', app.multer.fields([{ name: 'answer', maxCount: 1
         language: null,
         user_id: curUser.id,
         problem_id: req.params.id,
-        is_public: problem.is_public
+        is_public: problem.is_public,
+        user_rating: curUser.rating
       });
     } else {
       let code;
