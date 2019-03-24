3e3209ce0cc78f7adb9b600170b081f49089616a
Menci <huanghaorui301@gmail.com>
2019-03-24 14:27:17 +0800

    Fix highlighted code wrapped twice

--- libs/highlight.js
+++ libs/highlight.js
@@ -2,5 +2,7 @@ const { highlight } = require('syzoj-renderer');
 const objectHash = require('object-hash');
 
 module.exports = async (code, lang, cb) => {
-  highlight(code, lang, syzoj.redisCache).then(cb);
+  highlight(code, lang, syzoj.redisCache, {
+    wrapper: null
+  }).then(cb);
 }
