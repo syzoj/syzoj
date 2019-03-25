3ff2101f69d03561ccd9f98dec8ce248ab10a1a1
mektpoy <471811570@qq.com>
2019-03-26 00:34:20 +0800

    权限

--- modules/user.js
+++ modules/user.js
@@ -174,6 +174,8 @@ app.post('/user/:id/edit', async (req, res) => {
       if (!syzoj.utils.isValidUsername(req.body.username)) throw new ErrorMessage('无效的用户名。');
       user.username = req.body.username;
       user.email = req.body.email;
+      user.name = req.body.name;
+      user.school = req.body.school;
     }
 
     if (res.locals.user && res.locals.user.is_admin) {
@@ -189,8 +191,6 @@ app.post('/user/:id/edit', async (req, res) => {
 
     user.information = req.body.information;
     user.sex = req.body.sex;
-    user.name = req.body.name;
-    user.school = req.body.school;
     user.public_email = (req.body.public_email === 'on');
     user.prefer_formatted_code = (req.body.prefer_formatted_code === 'on');
 
