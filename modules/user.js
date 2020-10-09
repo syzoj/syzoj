e1e92b92219cab38fe061ea112eadb59380a4938
tianpeng2333 <tianpeng2333@gmail.com>
2020-10-09 16:17:46 +0800

    fix sign_up control

--- modules/user.js
+++ modules/user.js
@@ -59,8 +59,8 @@ app.get('/login', async (req, res) => {
 // Sign up
 app.get('/sign_up', async (req, res) => {
   if (!syzoj.config.open_sign_up){
-    res.render('error',{
-    err: new ErrorMessage('很遗憾，管理员暂未开启注册权限。请联系 OJ 管理员 @Tianpeng2333 开启注册权限。')
+    res.render('error', {
+      err: new ErrorMessage('管理员暂未开放注册权限')
     });
   }
   if (res.locals.user) {
