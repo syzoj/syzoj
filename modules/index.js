5dfa4b71e3a62b2f7573143e9f8f7878a6e139c5
tianpeng2333 <tianpeng2333@gmail.com>
2020-10-20 15:27:00 +0800

    add theme shop demo

--- modules/index.js
+++ modules/index.js
@@ -81,3 +81,15 @@ app.get('/calendar', async (req, res) => {
     });
   }
 });
+
+app.get('/theme_shop', async (req, res) => {
+  try {
+    if (!res.locals.user) throw new ErrorMessage('请登录后继续。', { '登录': syzoj.utils.makeUrl(['login'], { 'url': req.originalUrl }) });
+    res.render('calendar');
+  } catch (e) {
+    syzoj.log(e);
+    res.render('error', {
+      err: e
+    });
+  }
+});
