15d8689fe3c7ab0c071de1fcbc5068695e713417
tianpeng2333 <tianpeng2333@gmail.com>
2020-11-27 16:03:31 +0800

    fix signup bugs

--- modules/user.js
+++ modules/user.js
@@ -59,7 +59,7 @@ app.get('/login', async (req, res) => {
 // Sign up
 app.get('/sign_up', async (req, res) => {
   if (syzoj.config.open_sign_up === false){
-    res.render('error', {err: new ErrorMessage('管理员暂未开放注册权限')});
+    res.render('error', {err: new ErrorMessage('管理员暂未开放注册权限，请尝试登录。', { '登录': syzoj.utils.makeUrl(['login'], { 'url': req.originalUrl })})});
   }
   if (res.locals.user) {
     res.render('error', {
