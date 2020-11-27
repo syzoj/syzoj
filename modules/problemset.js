74ef6b3840f974dc2a92666bb371ce8c25ad1a7e
tianpeng2333 <tianpeng2333@gmail.com>
2020-11-27 16:38:27 +0800

    add problemset edit module

--- modules/problemset.js
+++ modules/problemset.js
@@ -31,3 +31,61 @@ app.get('/problemsets', async (req, res) => {
         });
     }
 });
+
+app.get('/problemset/:id/edit', async (req, res) => {
+    try{
+        if (!res.locals.user) throw new ErrorMessage('请登录后继续。', { '登录': syzoj.utils.makeUrl(['login'], { 'url': req.originalUrl }) });
+        if (!res.locals.user || !res.locals.user.is_admin) throw new ErrorMessage('您没有权限进行此操作。');
+        
+        let problemset_id = parseInt(req.params.id);
+        let problemset = await Problemset.findById(problemset_id);
+        if (!problemset){
+            problemset = await Problemset.create();
+            problemset.id = 0;
+        } 
+
+        let problems = [];
+        if (problemset.problems) problems = await problemset.problems.split('|').mapAsync(async id => await Problem.findById(id));
+        
+        res.render('problemset_edit', {
+            problemset: problemset,
+            problems: problems,
+        });
+    } catch (e){
+        syzoj.log(e);
+        res.render('error', {
+            err: e
+        });       
+    }
+});
+
+app.post('/problemset/:id/edit', async (req, res) => {
+    try{
+        if (!res.locals.user) throw new ErrorMessage('请登录后继续。', { '登录': syzoj.utils.makeUrl(['login'], { 'url': req.originalUrl }) });
+        if (!res.locals.user || !res.locals.user.is_admin) throw new ErrorMessage('您没有权限进行此操作。');
+
+        let problemset_id = parseInt(req.params.id);
+        let problemset = await Problemset.findById(problemset_id);
+
+        if(!problemset) {
+            problemset = await problemset.create();
+        }
+
+        if (!req.body.title.trim()) throw new ErrorMessage('题单名不能为空。');  
+        problemset.title = req.body.title;
+        problemset.subtitle = req.body.subtitle;
+        if (!Array.isArray(req.body.problems))  req.body.problems = [req.body.problems]
+        problemset.problems = req.body.problems.join('|');
+        problemset.information = req.body.information;
+        problemset.is_public = req.body.is_public === 'on';
+
+        await problemset.save();
+
+        res.redirect(syzoj.utils.makeUrl(['problemset', problemset.id]))
+    } catch (e){
+        syzoj.log(e);
+        res.render('error', {
+            err: e
+        });       
+    }
+});
